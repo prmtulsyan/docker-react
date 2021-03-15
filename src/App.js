eb7eecf9c87dd3a0e7b6eafdc5c20f884172c6b8
ptulsyan <prm.tulsyan@gmail.com>
2021-03-15 15:50:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Priyam Tulsyan
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Do not learn react!!
         </a>
       </header>
     </div>
